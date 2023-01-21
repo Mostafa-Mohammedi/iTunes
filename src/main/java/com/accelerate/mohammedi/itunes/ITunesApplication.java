eb07b1c0403b994cba7a2d759f6beb21fd44c7e8
Mostafa-Mohammedi <121897256+Mostafa-Mohammedi@users.noreply.github.com>
2023-01-21 19:07:43 +0100

    Initial commit

---
+++ src/main/java/com/accelerate/mohammedi/itunes/ITunesApplication.java
@@ -0,0 +1,13 @@
+package com.accelerate.mohammedi.itunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITunesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accelerate/mohammedi/itunes/ITunesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accelerate.mohammedi.itunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
